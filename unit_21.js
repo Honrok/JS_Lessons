3739ef0bae2d6942214cbe62829db0bd09a71f23
Svoikin Rostyslav <honrok261@gmail.com>
2021-09-30 04:22:10 +0200

    Lesson_21

---
+++ unit_21.js
@@ -0,0 +1,197 @@
+
+// Task 1 ============================================
+/* Создайте блок div-1. Добавьте на него событие touchstart. Выведите в out-1 слово  touch если событие сработает. */
+let out1 = document.querySelector('.out-1');
+function t1() {
+    out1.textContent = 'touch';
+}
+document.querySelector('.div-1').addEventListener('touchstart', t1);
+
+// ваше событие здесь!!!
+
+// Task 2 ============================================
+/* Создайте блок div-2. Добавьте на него событие touchstart. Выведите в out-2 число срабатываний события. */
+let out2 = document.querySelector('.out-2');
+let a = 0;
+function t2(event) {
+    a += event.touches.length;
+    out2.textContent = a;
+}
+document.querySelector('.div-2').addEventListener('touchstart', t2);
+
+// ваше событие здесь!!!
+
+
+// Task 3 ============================================
+/*  Создайте блок div-3_1 и div-3_2. Добавьте на них событие touchstart. Выведите в out-3 номер блока 1 или 2 на котором сработало событие. */
+let out3 = document.querySelector('.out-3');
+function t3() {
+    out3.textContent = '';
+    let a = this.getAttribute('class');
+    if (a == 'div-3_1 block') {
+        out3.textContent = 1;
+    } else if (a == 'div-3_2 block') {
+        out3.textContent = 2;
+    }
+}
+document.querySelector('.div-3_1').addEventListener('touchstart', t3);
+document.querySelector('.div-3_2').addEventListener('touchstart', t3);
+// ваше событие здесь!!!
+
+
+// Task 4 ============================================
+/*  Создайте блок div-4. И кнопку b-4. При нажатии кнопки - добавляйте событие ontouchstart на блок div-4. При событии происходит вывод текста touch в out-4.  */
+let out4 = document.querySelector('.out-4');
+function t4() {
+    out4.textContent = 'touch';
+}
+let div4 = document.querySelector('.div-4');
+document.querySelector('.b-4').onclick = () => { div4.addEventListener('touchstart', t4); }
+// ваше событие здесь!!!
+
+
+// Task 5 ============================================
+/*  Дана кнопка b-5. При ее нажатии очищайте событие ontouchstart на блоке div-4. */
+
+function t5() {
+    div4.removeEventListener('touchstart', t4);
+}
+document.querySelector('.b-5').onclick = t5;
+// ваше событие здесь!!!
+
+// Task 6 ============================================
+/*  Добавьте событие ontouchend на div-4. При его срабатывании выведите в out-6 слово touchend. */
+let out6 = document.querySelector('.out-6');
+function t6() {
+    out6.textContent = 'touchend';
+}
+div4.addEventListener('touchend', t6);
+// ваше событие здесь!!!
+
+
+// Task 7 ============================================
+/*  Дан блок div-7. Добавьте событие touch, при срабатывании которого окрашивайте блок в красный цвет. */
+
+function t7() {
+    a7.style.backgroundColor = 'red';
+}
+let a7 = document.querySelector('.div-7');
+a7.addEventListener('touchstart', t7);
+// ваше событие здесь!!!
+
+// Task 8 ============================================
+/*  Дан блок div-8. Добавьте на него событие touch, которое при срабатывании окрашивает блок случаным цветом из массива a8=[red, green, blue, orange, pink, yellow] */
+let a8 = ['red', 'green', 'blue', 'orange', 'pink', 'yellow'];
+function t8() {
+    // let color = Math.random()
+    let color = a8[Math.floor(Math.random() * a8.length)];
+    div8.style.backgroundColor = color;
+}
+let div8 = document.querySelector('.div-8');
+div8.addEventListener('touchstart', t8);
+// ваше событие здесь!!!
+
+
+// Task 9 ============================================
+/* Дан блок div-9. Добавьте событие ontouch. Выводите количество одновременных касаний в out-9. */
+let out9 = document.querySelector('.out-9');
+function t9(event) {
+    out9.textContent = event.touches.length;
+}
+document.querySelector('.div-9').addEventListener('touchstart', t9);
+// ваше событие здесь!!!
+
+
+// Task 10 ============================================
+/*  Дан блок div-10. Добавьте на него событие touchmove. При срабатывании события - увеличивайте его ширину на 1. */
+let w10 = 75;
+function t10() {
+    div10.style.width = w10 + 'px';
+    w10++;
+}
+let div10 = document.querySelector('.div-10');
+div10.addEventListener('touchmove', t10);
+
+// ваше событие здесь!!!
+
+// Task 11 ============================================
+/*  Дан блок div-11. Добавьте на него событие touch. При срабатывании выводите радиус события radiusX, radiusY. */
+let out11 = document.querySelector('.out-11');
+function t11(event) {
+    console.log(event);
+    let a = event.targetTouches[0].radiusX;
+    let b = event.targetTouches[0].radiusY;
+    out11.innerHTML = `radiusX: ${a} <br/> radiusY: ${b}`;
+}
+document.querySelector('.div-11').addEventListener('touchstart', t11);
+
+// ваше событие здесь!!!
+
+// Task 12 ============================================
+/*  Мини проект. Ознакомьтесь с версткой в задании 12.
+<p>Изучите html код внутри div-12-wrapper.</p>
+<p>1. Добавьте на кнопку nex событие click, touch так, чтобы при событии запускалась функция nextFunction и активным становилось изображение следующее за выделенным классом active-img (рамкой). Соответственно, на активном изображении появляется рамка, а остальные - лишаются рамки.</p>
+<p>2. Добавьте на кнопку prev событие click, touch так, чтобы при событии запускалась функция prevFunction и активным становилось изображение до выделенного классом active-img (рамкой). Соответственно, на активном изображении появляется рамка, а остальные - лишаются рамки.</p>
+<p>3. Учтите краевые эффекты - когда мы доходим до конца или начала, то нажатие кнопки должно приводить к перемещению рамки в начало или конец изображений.</p>
+<p>4. Добавьте кнопку reset (функция resetFunction), нажатие которой сбрасывает активное изображение на нулевое. </p>
+<p>5. Добавьте во все действия следующее - в изображении img-12-max заменяется src на активную. Т.е. произошло событие - заменилась главная картинка.</p>
+*/
+
+const images = document.querySelectorAll('.img-12-min');
+let count = 0; // переменная, которая указывает на номер активного изображения в images
+
+const next = document.querySelector('.next');
+next.onclick = nextFunction;
+
+const prev = document.querySelector('.prev');
+prev.onclick = prevFunction;
+
+const reset = document.querySelector('.reset');
+reset.onclick = resetFunction;
+
+let div12 = document.querySelector('.div-12-max img');
+
+function nextFunction() {
+    for (let i = 0; i < images.length; i++) {
+        if ((images[i].classList.contains('active-img') == true) && i != images.length - 1) {
+            images[i].classList.remove('active-img');
+            images[i + 1].classList.add('active-img');
+            div12.setAttribute('src', images[i + 1].getAttribute('src'));
+            break;
+        } else if ((images[i].classList.contains('active-img') == true) && i == images.length - 1) {
+            images[i].classList.remove('active-img');
+            images[0].classList.add('active-img');
+            div12.setAttribute('src', images[0].getAttribute('src'));
+            break;
+        }
+    }
+}
+
+function prevFunction() {
+    for (let i = 0; i < images.length; i++) {
+        if ((images[i].classList.contains('active-img') == true) && i != 0) {
+            images[i].classList.remove('active-img');
+            images[i - 1].classList.add('active-img');
+            div12.setAttribute('src', images[i - 1].getAttribute('src'));
+            break;
+        } else if ((images[i].classList.contains('active-img')) && i == 0) {
+            images[i].classList.remove('active-img');
+            images[images.length - 1].classList.add('active-img');
+            div12.setAttribute('src', images[images.length - 1].getAttribute('src'));
+            break;
+        }
+    }
+}
+
+function resetFunction() {
+    for (let i = 0; i < images.length; i++) {
+        if (images[i].classList.contains('active-img') == true) {
+            images[i].classList.remove('active-img');
+            images[0].classList.add('active-img');
+            div12.setAttribute('src', images[0].getAttribute('src'));
+            break;
+        }
+    }
+}
+
+// ваше событие здесь!!!
